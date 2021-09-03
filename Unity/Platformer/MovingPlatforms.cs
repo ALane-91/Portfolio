62382a3963158b4ebb74080128a9170dc06ff651
Andrew Lane <86128477+ALane-91@users.noreply.github.com>
2021-09-03 14:39:54 +0100

    Add files via upload

---
+++ Unity/Platformer/MovingPlatforms.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public Transform[] Points;
+
+    /*Create platform paths in Unity by creating a game object and assigning as many waypoints as desired as children to this game object.
+    Create game object, rename to platform pathing, or something memorable.
+    Create a second game object rename to "Platform". Use this to create a prefab to avoid recreating the platform each use. 
+    Set desired platform block as a child of this. Position child so parent is centralised. Attach Script to parent, not the child.*/
+
+    public IEnumerator<Transform> GetPathEnumerator()
+    {
+        //requires at least one point after the starting position to make a path, if there is none the script won't run.
+        if (Points == null || Points.Length < 1)
+            yield break;
+
+        var direction = 1;
+        var index = 0;
+        
+        while (true)
+        {
+            yield return Points[index];
+
+            if (index <= 0)
+                direction = 1;
+            else if (index >= Points.Length - 1)
+                direction = -1;
+
+            index = index + direction;
+        }
+
+    }
+    //Built in Unity tool for handling objects.
+    public void OnDrawGizmos()
+    {
+        //Required as the Points array isn't instantiated without a value, meaning null is required.
+        if (Points == null || Points.Length < 2)
+            return;
+
+        for (var i = 1; i < Points.Length; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+        }
+    }
+}
+
---
+++ Unity/Platformer/PathFollowing.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollowing : MonoBehaviour
+{
+    public enum FollowType
+    {
+        MoveTowards,
+        Back
+    }
+
+    //Sets the default FollowType to be MoveTowards;
+    public FollowType Type = FollowType.MoveTowards;
+    //Refers back to the MovingPlatforms script
+    public MovingPlatforms Path;
+    public float Speed = 1;
+    //If within a certain range of the next point in path, will start moving towards other the point after.
+    public float MaxDistanceToGoal = .1f;
+
+    private IEnumerator<Transform> _currentPoint;
+
+    public void Start()
+    {
+        if (Path == null)
+        {
+            //Sends an error to console if value of Path is null, e.g if no start point is set for a path.
+            Debug.LogError("Path cannot be null", gameObject);
+            return;
+        }
+
+        _currentPoint = Path.GetPathEnumerator();
+        _currentPoint.MoveNext();
+
+        if (_currentPoint.Current == null)
+            return;
+
+        transform.position = _currentPoint.Current.position;
+    }
+
+    public void Update()
+    {
+        if (_currentPoint == null || _currentPoint.Current == null)
+            return;
+
+        if (Type == FollowType.MoveTowards)
+            transform.position = Vector3.MoveTowards(transform.position, _currentPoint.Current.position, Time.deltaTime * Speed);
+        else if (Type == FollowType.Back)
+            transform.position = Vector3.Lerp(transform.position, _currentPoint.Current.position, Time.deltaTime * Speed);
+
+        var distanceSquared = (transform.position - _currentPoint.Current.position).sqrMagnitude;
+        if (distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal)
+            _currentPoint.MoveNext();
+    }
+}
